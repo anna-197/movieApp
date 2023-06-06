048a13e5ed2ce48f80887d444075ad0ac9a1f73f
Ananya Nair <an.nair1702@gmail.com>
2023-06-06 19:47:41 +0530

    Styled data into grids

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,12 @@ const Header = () => {
   return (
     <>
       <div className="header">
-        {/* anchor tag for loading home page on clicking the image */}
-        <a href="/">
+        
           <img
-            
+            src='https://static.vecteezy.com/system/resources/previews/021/669/876/non_2x/video-camera-gold-icon-illustration-of-golden-particle-background-gold-icon-vector.jpg'
             alt="logo"
-          ></img>
-        </a>
+          />
+        
         <h1>Welcome to Movies Flix</h1>
         <ul className="nav-items">
           {/* <SearchBar/> */}
